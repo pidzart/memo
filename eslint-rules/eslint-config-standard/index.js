58afe11b4b548f7f9f8c34bfada4c9e7c3b3e98d
ymk <65121726+mikiymk@users.noreply.github.com>
2022-08-16 11:34:09 +0900

    Create index.js

---
+++ eslint-rules/eslint-config-standard/index.js
@@ -0,0 +1,16 @@
+# eslint-config-standard
+
+latest version 17.0.0 published at
+
+[npm](https://www.npmjs.com/package/eslint-config-standard)
+[github](https://github.com/standard/eslint-config-standard)
+
+## plugins used
+
+- eslint-plugin-import
+- eslint-plugin-n
+- eslint-plugin-promise
+
+## rule configs
+
+- standard
